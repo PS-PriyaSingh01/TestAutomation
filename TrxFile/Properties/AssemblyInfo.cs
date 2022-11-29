80b8071ad0638db6fa290ad98adfb2221472bdaa
PS-PriyaSingh01 <99895674+PS-PriyaSingh01@users.noreply.github.com>
2022-11-29 12:44:10 +0530

    master

--- TrxFile/Properties/AssemblyInfo.cs
+++ TrxFile/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ConsoleApp1")]
+[assembly: AssemblyTitle("TrxFile")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ConsoleApp1")]
+[assembly: AssemblyProduct("TrxFile")]
 [assembly: AssemblyCopyright("Copyright ©  2022")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("540d821f-c867-4698-9833-ebad5d293934")]
+[assembly: Guid("0a8cae29-06d2-4a84-8869-c78eaf3d0b13")]
 
 // Version information for an assembly consists of the following four values:
 //
